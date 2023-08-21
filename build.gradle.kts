96a545d2cc384b12ab1981361009166ac1922cec
Vadik Sirekanyan <me@sirekanyan.org>
2023-08-21 23:23:12 +0400

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     kotlin("android") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion apply false
     id("app.cash.sqldelight") version "2.0.0" apply false
-    id("org.sirekanyan.version-checker") version "1.0.7" apply false
+    id("org.sirekanyan.version-checker") version "1.0.8" apply false
 }
