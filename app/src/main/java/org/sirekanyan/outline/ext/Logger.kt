f74578fcd49c55d98b7ad34eee48ec4f05489293
Vadik Sirekanyan <me@sirekanyan.org>
2023-11-25 22:19:17 +0400

    Added skipping cancellation exception

--- app/src/main/java/org/sirekanyan/outline/ext/Logger.kt
+++ app/src/main/java/org/sirekanyan/outline/ext/Logger.kt
@@ -2,20 +2,29 @@ package org.sirekanyan.outline.ext
 
 import android.util.Log
 import org.sirekanyan.outline.CrashReporter
+import java.util.concurrent.CancellationException
 
 private const val TAG = "Outline"
 
 fun logDebug(message: String, throwable: Throwable?) {
-    CrashReporter.handleException(throwable ?: Exception(message))
+    sendCrashReport(message, throwable)
     Log.d(TAG, message, throwable)
 }
 
 fun logWarn(message: String, throwable: Throwable?) {
-    CrashReporter.handleException(throwable ?: Exception(message))
+    sendCrashReport(message, throwable)
     Log.w(TAG, message, throwable)
 }
 
 fun logError(message: String, throwable: Throwable?) {
-    CrashReporter.handleException(throwable ?: Exception(message))
+    sendCrashReport(message, throwable)
     Log.e(TAG, message, throwable)
+}
+
+private fun sendCrashReport(message: String, throwable: Throwable?) {
+    if (throwable is CancellationException) {
+        Log.d(TAG, "Skipped sending crash report")
+    } else {
+        CrashReporter.handleException(throwable ?: Exception(message))
+    }
 }
